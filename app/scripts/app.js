4c3ee2a22d5f5299e9aa10a02e06ae82d1a73503
spitimage <aaron.nielsen@blooco.com>
2013-11-27 15:57:02 -0700

    Make typeahead view default

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,10 +4,6 @@ angular.module('meanTypeaheadApp', ['ngRoute','ui.bootstrap'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/typeahead', {
         templateUrl: 'views/typeahead.html',
         controller: 'TypeaheadCtrl'
       })
