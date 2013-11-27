78ecc52c9f147cba53a6f3a862f7e42feeef02bd
spitimage <aaron.nielsen@blooco.com>
2013-11-27 14:16:31 -0700

    Add CORS to make demo easier

--- server/server.js
+++ server/server.js
@@ -9,6 +9,21 @@ app.use(express.logger());
 // Serve up the front-side build
 app.use(express.static('../dist'));
 
+// Enable CORS to support running from the dev server (port 9000)
+app.use(function (req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+
+// Part of CORS
+app.options("*", function(req, res, next){
+  res.send(200);
+});
+
+
+
 // Create our routes
 app.get('/api/widgets', widgets.regex);
 
