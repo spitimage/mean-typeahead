a0b1be87a2cf0dd3faa79cdbc4526bcce95f5823
spitimage <aaron.nielsen@blooco.com>
2013-11-27 11:21:14 -0700

    Initial yeoman scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanTypeaheadApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
