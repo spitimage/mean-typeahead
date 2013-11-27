c0f5f2f46cad780764cbc7a3fcd63cb9e658a66d
spitimage <aaron.nielsen@blooco.com>
2013-11-27 14:38:54 -0700

    Wire typeahead to API

--- app/scripts/controllers/typeahead.js
+++ app/scripts/controllers/typeahead.js
@@ -1,8 +1,24 @@
 'use strict';
 
 angular.module('meanTypeaheadApp')
-  .controller('TypeaheadCtrl', function ($scope, $http) {
-    // Fallback data in case we don't have the backend running or we're in dev mode
-    $scope.items = ['apples','oranges','bananas','carrots','celery'];
+  .controller('TypeaheadCtrl', function($scope, $http) {
+    $scope.selected = '';
+    $scope.items = [];
+
+    // Make a new query every time the search field is updated
+    $scope.$watch('selected', function(newVal) {
+      if (newVal) {
+        $http.get('http://localhost:3000/api/widgets?q=' + newVal).then(function(data) {
+          $scope.items = data.data;
+        }, function(err) {
+          console.log(err);
+        });
+      }
+    });
+
+    $scope.onSelect = function() {
+      console.log('Selected:', $scope.selected);
+    };
 
   });
+
